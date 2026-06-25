a7f856368671c95d84d25fd6af29ca2caf138a87
Harsh Prajapat <harshprajapat132100@gmail.com>
2026-06-25 13:34:29 +0530

    Update team members for 2026-27

--- src/data/teamData.jsx
+++ src/data/teamData.jsx
@@ -3,269 +3,275 @@ export const teamData = {
   subtitle: "The brilliant minds behind Aerove's innovative drone technology solutions",
   members: [
     {
-      id: 1,
-      name: "Jainam",
-      role: "OC",
+      id:34,
+      name: "Manav",
+      role: "Overall Coordinator",
       department: "Leadership",
-      avatar: "photos/Jainam.jpeg",
-      social: { instagram: "#", linkedin: "#" }
+      avatar: "photos_26-27/Manav.jpeg",
+      social: {instagram : "https://www.instagram.com/_manav_1405__/", linkedin: "https://www.linkedin.com/in/manav-parmar-675544293/"}
+
+    },
+   
+    {
+      id: 1,
+      name: "Rohan",
+      role: "Team Lead",
+      department: "Leadership",
+      avatar: "photos_26-27/Rohan.jpeg",
+      social: { instagram: "https://www.instagram.com/rohanjoshi_2105/", linkedin: "https://www.linkedin.com/in/rohan-joshi-845a16330/" }
     },
     {
       id: 2,
-      name: "Prashant",
-      role: "Manager",
-      department: "Management",
-      avatar: "photos/Prashant.jpg",
-      social: { instagram: "#", linkedin: "#" }
+      name: "Daksh Sawke",
+      role: "Team Lead",
+      department: "Leadership",
+      avatar: "photos_26-27/DakshSawke.jpg",
+      social: { instagram: "#", linkedin: "https://www.linkedin.com/in/daksh-sawke" }
     },
     {
       id: 3,
-      name: "Jeet",
+      name: "Nishit",
       role: "Manager",
       department: "Management",
-      avatar: "photos/Jeet.jpeg",
-      social: { instagram: "#", linkedin: "#" }
+      avatar: "photos_26-27/Nishit.png",
+      social: { instagram: "https://www.instagram.com/nishitdharamshi", linkedin: "https://www.linkedin.com/in/nishit-dharamshi-31886333a" }
     },
     {
       id: 4,
-      name: "Anjali",
+      name: "Premansh",
       role: "Manager",
       department: "Management",
-      avatar: "photos/Anjali.jpeg",
-      social: { instagram: "#", linkedin: "#" }
+      avatar: "photos_26-27/Premansh.jpg",
+      social: { instagram: "https://www.instagram.com/43premansh", linkedin: "https://www.linkedin.com/in/43premansh" }
     },
     {
       id: 5,
-      name: "Viraj",
-      role: "Manager",
-      department: "Management",
-      avatar: "photos/Viraj.jpg",
-      social: { instagram: "#", linkedin: "#" }
+      name: "Sambhav",
+      role: "Subsystem Lead",
+      department: "Mechatronics Subsystem",
+      avatar: "photos_26-27/Sambhav.png",
+      social: { instagram: "https://www.instagram.com/polestar2025", linkedin: "https://www.linkedin.com/in/sambhav-jha-22445b262" }
     },
     {
       id: 6,
-      name: "Rohan",
-      role: "Lead",
+      name: "Nipun",
+      role: "Subsystem Lead",
       department: "Mechatronics Subsystem",
-      avatar: "photos/Rohan.jpeg",
-      social: { instagram: "#", linkedin: "#" }
+      avatar: "photos_26-27/Nipun.jpg",
+      social: { instagram: "https://www.instagram.com/nipun.nistane", linkedin: "https://www.linkedin.com/in/nipun-nistane-7a0bb5312" }
     },
     {
       id: 7,
-      name: "Divig",
-      role: "Senior Engineer",
-      department: "Mechatronics Subsystem",
-      avatar: "photos/Divig.jpeg",
+      name: "Durva",
+      role: "Subsystem Lead",
+      department: "Aerodynamics Subsystem",
+      avatar: "photos_26-27/Durva.jpg",
+      social: { instagram: "https://www.instagram.com/dpg_1937", linkedin: "https://www.linkedin.com/in/durva-gandharva-164405388" }
+    },
+    
+    {
+      id: 10,
+      name: "Arpit Kumar",
+      role: "Subsystem Lead",
+      department: "MPC Subsystem",
+      avatar: "photos_26-27/ArpitKumar.jpg",
       social: { instagram: "#", linkedin: "#" }
+    },
+     {
+      id: 16,
+      name: "Narendra",
+      role: "Subsystem Lead",
+      department: "Perception Subsystem",
+      avatar: "photos_26-27/Narendra.jpeg",
+      social: { instagram: "https://www.instagram.com/n_r_ndr_/", linkedin: "https://www.linkedin.com/in/narendra-aironi/" }
+    },
+    {
+      id: 17,
+      name: "Aarav Gupta",
+      role: "Subsystem Lead",
+      department: "Perception Subsystem",
+      avatar: "photos_26-27/AaravGupta.jpg",
+      social: { instagram: "#", linkedin: "https://www.linkedin.com/in/aarav-gupta-128282202/" }
     },
     {
       id: 8,
-      name: "Jahnavi",
-      role: "Member",
-      department: "Mechatronics Subsystem",
-      avatar: "photos/Jahnavi.jpg",
-      social: { instagram: "#", linkedin: "#" }
-    },
-    {
-      id: 9,
-      name: "Sambhav",
-      role: "Member",
-      department: "Mechatronics Subsystem",
-      avatar: "photos/Sambhav.jpeg",
-      social: { instagram: "#", linkedin: "#" }
-    },
-    {
-      id: 10,
-      name: "Manav",
-      role: "Lead",
-      department: "Aerodynamic Subsystem",
-      avatar: "photos/Manav.png",
-      social: { instagram: "#", linkedin: "#" }
+      name: "Dhruv",
+      role: "Senior Engineer",
+      department: "Aerodynamics Subsystem",
+      avatar: "photos_26-27/Dhruv.jpg",
+      social: { instagram: "https://www.instagram.com/dhruv_jadhav_45", linkedin: "https://www.linkedin.com/in/dhruv-jadhav-b95407318" }
     },
     {
       id: 11,
-      name: "Prabhakar",
+      name: "Basant",
       role: "Senior Engineer",
-      department: "Aerodynamic Subsystem",
-      avatar: "photos/Prabhakar.jpeg",
+      department: "MPC Subsystem",
+      avatar: "photos_26-27/Basant.jpeg",
       social: { instagram: "#", linkedin: "#" }
     },
     {
       id: 12,
-      name: "Vijay",
-      role: "Member",
-      department: "Aerodynamic Subsystem",
-      avatar: "photos/Vijay.jpg",
+      name: "Naman",
+      role: "Senior Engineer",
+      department: "MPC Subsystem",
+      avatar: "photos_26-27/Naman.jpeg",
+      social: { instagram: "#", linkedin: "#" }
+    },
+     {
+      id: 13,
+      name: "Krutarth",
+      role: "Senior Engineer",
+      department: "MPC Subsystem",
+      avatar: "photos_26-27/Krutarth.jpeg",
       social: { instagram: "#", linkedin: "#" }
     },
     {
-      id: 13,
-      name: "Durva",
-      role: "Member",
-      department: "Aerodynamic Subsystem",
-      avatar: "photos/Durva.jpeg",
-      social: { instagram: "#", linkedin: "#" }
+      id: 9,
+      name: "Devangi",
+      role: "Junior Engineer",
+      department: "Aerodynamics Subsystem",
+      avatar: "photos_26-27/Devangi.jpeg",
+      social: { instagram: "https://www.instagram.com/devi.lilvelcro", linkedin: "https://www.linkedin.com/in/devangi-chaudhuri-0a584b242/" }
     },
+    
+   
     {
       id: 14,
-      name: "Nipun",
-      role: "Member",
-      department: "Aerodynamic Subsystem",
-      avatar: "photos/Nipun.jpeg",
-      social: { instagram: "#", linkedin: "#" }
+      name: "Vipul Bansal",
+      role: "Junior Engineer",
+      department: "MPC Subsystem",
+      avatar: "photos_26-27/VipulBansal.jpeg",
+      social: { instagram: "https://www.instagram.com/_vipul_957_/", linkedin: "https://www.linkedin.com/in/vipul-bansal-8b1344289" }
     },
     {
       id: 15,
-      name: "Gokul",
-      role: "Lead",
+      name: "Abha",
+      role: "Junior Engineer",
       department: "MPC Subsystem",
-      avatar: "photos/Gokul.jpeg",
-      social: { instagram: "#", linkedin: "#" }
+      avatar: "photos_26-27/Abha.jpeg",
+      social: { instagram: "https://www.instagram.com/abhas_2007/", linkedin: "https://www.linkedin.com/in/abha-shelke-835430390/" }
     },
-    {
-      id: 16,
-      name: "Deepak",
-      role: "Senior Engineer",
-      department: "MPC Subsystem",
-      avatar: "photos/Deepak.jpeg",
-      social: { instagram: "#", linkedin: "#" }
-    },
-    {
-      id: 17,
-      name: "Vihang",
-      role: "Member",
-      department: "MPC Subsystem",
-      avatar: "photos/Vihang.jpeg",
-      social: { instagram: "#", linkedin: "#" }
-    },
+   
     {
       id: 18,
-      name: "Basant",
-      role: "Member",
-      department: "MPC Subsystem",
-      avatar: "photos/Basant.jpeg",
-      social: { instagram: "#", linkedin: "#" }
+      name: "Cheriyan",
+      role: "Junior Engineer",
+      department: "Perception Subsystem",
+      avatar: "photos_26-27/Cheriyan.jpg",
+      social: { instagram: "#", linkedin: "https://www.linkedin.com/in/cheriyanr200779" }
     },
     {
       id: 19,
-      name: "Anushka",
-      role: "Member",
-      department: "MPC Subsystem",
-      avatar: "photos/Anushka.jpeg",
-      social: { instagram: "#", linkedin: "#" }
-    },
-    {
-      id: 20,
-      name: "Manan",
-      role: "Member",
-      department: "MPC Subsystem",
-      avatar: "photos/Manan.png",
-      social: { instagram: "#", linkedin: "#" }
-    },
-    {
-      id: 21,
-      name: "Arpit",
-      role: "Member",
-      department: "MPC Subsystem",
-      avatar: "photos/Arpit.jpg",
-      social: { instagram: "#", linkedin: "#" }
-    },
-    {
-      id: 22,
-      name: "Daksh",
-      role: "Senior Engineer",
+      name: "Vidit",
+      role: "Junior Engineer",
       department: "Perception Subsystem",
-      avatar: "photos/Daksh.jpeg",
-      social: { instagram: "#", linkedin: "#" }
-    },
-    {
-      id: 23,
-      name: "Sujay",
-      role: "Senior Engineer",
-      department: "Perception Subsystem",
-      avatar: "photos/Sujay.jpg",
-      social: { instagram: "#", linkedin: "#" }
+      avatar: "photos_26-27/Vidit.jpeg",
+      social: { instagram: "#", linkedin: "https://www.linkedin.com/in/vidit-nagpurkar/" }
     },
     {
       id: 24,
-      name: "Aarav",
-      role: "Member",
-      department: "Perception Subsystem",
-      avatar: "photos/Aarav.jpeg",
-      social: { instagram: "#", linkedin: "#" }
+      name: "Afnan Ahmed",
+      role: "Junior Engineer",
+      department: "Phase 2 freshie",
+      avatar: "photos_26-27/AfnanAhmed.jpg",
+      social: { instagram: "https://www.instagram.com/afnanahmed450", linkedin: "https://www.linkedin.com/in/afnan-ahmed-449680374" }
     },
     {
       id: 25,
-      name: "Megha",
-      role: "Member",
-      department: "Perception Subsystem",
-      avatar: "photos/Megha.jpg",
-      social: { instagram: "#", linkedin: "#" }
+      name: "Tathagata",
+      role: "Junior Engineer",
+      department: "Phase 2 freshie",
+      avatar: "photos_26-27/Tathagata.png",
+      social: { instagram: "#", linkedin: "https://www.linkedin.com/in/tathagata-roy-0a1b1a20a" }
     },
     {
       id: 26,
-      name: "Narendra",
-      role: "Member",
-      department: "Perception Subsystem",
-      avatar: "photos/Narendra.jpg",
-      social: { instagram: "#", linkedin: "#" }
+      name: "Bhavya Patel",
+      role: "Junior Engineer",
+      department: "Phase 2 freshie",
+      avatar: "photos_26-27/BhavyaPatel.jpeg",
+      social: { instagram: "https://www.instagram.com/_.bhavya.patel_", linkedin: "https://www.linkedin.com/in/bhavya-patel-64014b377/" }
     },
     {
       id: 27,
-      name: "Maitreyee",
-      role: "Member",
-      department: "Perception Subsystem",
-      avatar: "photos/Maitreyee.jpg",
-      social: { instagram: "#", linkedin: "#" }
+      name: "Shreeya",
+      role: "Junior Engineer",
+      department: "Phase 2 freshie",
+      avatar: "photos_26-27/Shreeya.jpeg",
+      social: { instagram: "https://www.instagram.com/nair_shreeya/", linkedin: "https://www.linkedin.com/in/shreeya-nair-780a63417" }
     },
     {
       id: 28,
-      name: "Joshua",
-      role: "Member",
-      department: "Perception Subsystem",
-      avatar: "photos/Joshua.jpg",
-      social: { instagram: "#", linkedin: "#" }
+      name: "Parth Rane",
+      role: "Junior Engineer",
+      department: "Phase 2 freshie",
+      avatar: "photos_26-27/ParthRane.jpeg",
+      social: { instagram: "https://www.instagram.com/parthrane4/", linkedin: "https://www.linkedin.com/in/parth-rane-982622372/" }
     },
     {
       id: 29,
-      name: "Ishwar",
-      role: "Finance & Logistics Member",
-      department: "Business Team",
-      avatar: "photos/Ishwar.jpg",
-      social: { instagram: "#", linkedin: "#" }
+      name: "Raunak Raj",
+      role: "Junior Engineer",
+      department: "Phase 2 freshie",
+      avatar: "photos_26-27/RaunakRaj.jpg",
+      social: { instagram: "https://www.instagram.com/__newton_raunak", linkedin: "https://www.linkedin.com/in/raunak-raj-89888a313" }
     },
     {
       id: 30,
-      name: "Preetham",
-      role: "Finance & Logistics Member",
-      department: "Business Team",
-      avatar: "photos/Preetham.jpg",
-      social: { instagram: "#", linkedin: "#" }
+      name: "Gaurav",
+      role: "Junior Engineer",
+      department: "Phase 2 freshie",
+      avatar: "photos_26-27/Gaurav.jpeg",
+      social: { instagram: "https://www.instagram.com/gaurav.m_08/", linkedin: "https://www.linkedin.com/in/gaurav-maharana-377453418" }
     },
     {
       id: 31,
-      name: "Srinidhi",
-      role: "Web Dev Member",
-      department: "Business Team",
-      avatar: "photos/Srinidhi.jpg",
-      social: { instagram: "#", linkedin: "#" }
+      name: "Parth Lohiya",
+      role: "Junior Engineer",
+      department: "Phase 2 freshie",
+      avatar: "photos_26-27/ParthLohiya.jpeg",
+      social: { instagram: "https://www.instagram.com/lohiya_parth/", linkedin: "https://www.linkedin.com/in/parthlohiya" }
     },
     {
-      id: 32,
-      name: "Kovid",
-      role: "Web Dev Member",
+      id: 20,
+      name: "Satyam",
+      role: "Finance and Logistics Member",
       department: "Business Team",
-      avatar: "photos/Kovid.jpg",
-      social: { instagram: "#", linkedin: "#" }
+      avatar: "photos_26-27/Satyam.webp",
+      social: { instagram: "https://www.instagram.com/satyamyeola/", linkedin: "https://www.linkedin.com/in/satyamyeola/" }
     },
     {
-      id: 33,
-      name: "Premansh",
-      role: "Media & PR Member",
+      id: 21,
+      name: "Divyansh",
+      role: "Finance and Logistics Member",
       department: "Business Team",
-      avatar: "photos/Premansh.jpg",
-      social: { instagram: "#", linkedin: "#" }
+      avatar: "photos_26-27/Divyansh.jpeg",
+      social: { instagram: "https://www.instagram.com/divyansh.singh_38", linkedin: "https://www.linkedin.com/in/divyansh-singh-788491393" }
+    },
+    {
+      id: 22,
+      name: "Navinya Desai",
+      role: "Finance and Logistics Member",
+      department: "Business Team",
+      avatar: "photos_26-27/NavinyaDesai.jpg",
+      social: { instagram: "https://www.instagram.com/navinya_27", linkedin: "https://www.linkedin.com/in/navinya-desai-a76086376" }
+    },
+    {
+      id: 23,
+      name: "Parth Ingle",
+      role: "Media and PR Member",
+      department: "Business Team",
+      avatar: "photos_26-27/ParthIngle.jpeg",
+      social: { instagram: "https://www.instagram.com/_parth_i7_/", linkedin: "https://www.linkedin.com/in/parth-ingle-84961a374/" }
+    },
+    {
+      id: 35,
+      name:"Harsh",
+      role:"Web Developer",
+      department: "Business Team",
+      avatar: "photos_26-27/Harsh.jpeg",
+      social : {instagram: "https://www.instagram.com/harsh_.prjpt/", linkedin: "https://www.linkedin.com/in/harsh-prajapat-a39341386/"},
     },
   ]
+  
 }
-
